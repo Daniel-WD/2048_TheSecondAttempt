4c71bb01298a40ea3f91ba4688758743e30622f2
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2018-09-21 15:44:59 +0200

    Some play service things(which don't work)

--- app/src/main/java/com/titaniel/best_2048_math_puzzle/MainActivity.java
+++ app/src/main/java/com/titaniel/best_2048_math_puzzle/MainActivity.java
@@ -1,24 +1,34 @@
 package com.titaniel.best_2048_math_puzzle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.titaniel.best_2048_math_puzzle.admob.Admob;
 import com.titaniel.best_2048_math_puzzle.database.Database;
 import com.titaniel.best_2048_math_puzzle.database.DesignProvider;
-import com.titaniel.best_2048_math_puzzle.fragments.dialog.GameOver;
-import com.titaniel.best_2048_math_puzzle.fragments.dialog.Undo;
-import com.titaniel.best_2048_math_puzzle.fragments.dialog.Pause;
-import com.titaniel.best_2048_math_puzzle.fragments.dialog.Won;
-import com.titaniel.best_2048_math_puzzle.fragments.game.Game;
 import com.titaniel.best_2048_math_puzzle.fragments.AnimatedFragment;
 import com.titaniel.best_2048_math_puzzle.fragments.Home;
+import com.titaniel.best_2048_math_puzzle.fragments.dialog.GameOver;
+import com.titaniel.best_2048_math_puzzle.fragments.dialog.Pause;
+import com.titaniel.best_2048_math_puzzle.fragments.dialog.Undo;
+import com.titaniel.best_2048_math_puzzle.fragments.dialog.Won;
+import com.titaniel.best_2048_math_puzzle.fragments.game.Game;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 12;
 
     //states
     public static final int
@@ -44,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler mAdmobHandler;
 
+    private GoogleSignInAccount mGoogleSignAccount;
+    private GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
         mAdmobHandler = new Handler(thread.getLooper());
 //        mAdmobHandler.post(() -> {
-            //admob
-            Admob.init(this, mHandler);
+        //admob
+        Admob.init(this, mHandler);
 //        });
 
         //Database
@@ -62,6 +75,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Design Provider
         DesignProvider.init(this);
+
+        //signinclient
+        mGoogleSignInClient = GoogleSignIn.getClient(this,
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build());
 
         //init Fragments
         home = (Home) getSupportFragmentManager().findFragmentById(R.id.fragmentHome);
@@ -72,7 +89,45 @@ public class MainActivity extends AppCompatActivity {
         won = (Won) getSupportFragmentManager().findFragmentById(R.id.fragmentWon);
 
         mHandler.postDelayed(() -> showState(STATE_FM_HOME, 0, null), 500);
+//        mHandler.postDelayed(this::signInSilently, 500);
+    }
 
+    private void signInSilently() {
+        mGoogleSignInClient.silentSignIn().addOnCompleteListener(this,
+                task -> {
+                    if(task.isSuccessful()) {
+                        mGoogleSignAccount = task.getResult();
+                    } else {
+                        startSignInIntent();
+                    }
+                });
+    }
+
+    private void startSignInIntent() {
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                mGoogleSignAccount = task.getResult(ApiException.class);
+            } catch (ApiException apiException) {
+
+                String message = apiException.getMessage();
+                if(message == null || message.isEmpty()) {
+                    message = "määääääaännnn";
+                }
+
+                new AlertDialog.Builder(this)
+                        .setMessage(message)
+                        .setNeutralButton(android.R.string.ok, null)
+                        .show();
+            }
+        }
     }
 
     public void showState(int newState, long delay, @Nullable AnimatedFragment oldFragment) {
@@ -117,7 +172,6 @@ public class MainActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
 
         Database.load();
-
 
         mHandler.post(() -> {
             if(Admob.rewardedVideoAd != null) Admob.rewardedVideoAd.resume(this);
