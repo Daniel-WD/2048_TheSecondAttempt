89f68355882b02c847f989a52a5a2730b518fb3b
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2018-10-18 22:20:26 +0200

    never do this again --> delete unused stuff

--- app/src/main/java/com/titaniel/best_2048_math_puzzle/leaderboard_manager/LeaderboardManager.java
+++ app/src/main/java/com/titaniel/best_2048_math_puzzle/leaderboard_manager/LeaderboardManager.java
@@ -1,10 +1,18 @@
 package com.titaniel.best_2048_math_puzzle.leaderboard_manager;
 
+import androidx.appcompat.widget.AppCompatTextView;
+
 public class LeaderboardManager {
 
-    public class LeaderboardInstance {
+    public static class LeaderboardInstance {
 
+        //rank textviews
+        private AppCompatTextView tvRankTodayHighscore, tvRankTodayTileRecord,
+                tvRank7DaysHighscore, tvRank7DaysTileRecord,
+                tvRankAllTimeHighscore, tvRankAllTimeTileRecord;
 
+        //indicators (up or down)
+        private ImageView
 
     }
 
